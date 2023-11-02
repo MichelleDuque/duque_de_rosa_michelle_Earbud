06f6bf25422a7d516636c84737e84549742438f1
MichelleDuque <michelled_1994@hotmail.com>
2023-11-02 01:08:22 -0400

    gsap animations were adeed to hero image, info box and phrase box

--- js/burger.js
+++ js/burger.js
@@ -2,15 +2,27 @@
     
 const menu_btn = document.querySelector('.hamburger');
 const mobile_menu = document.querySelector('.mobile-nav');
-let hamburger_image = document.querySelector("#hamburger-image");
+const hamburger_image = document.querySelector("#hamburger-image");
+const mobile_nav = document.querySelector(".mobile-nav")
 
 menu_btn.addEventListener('click', function () {
   menu_btn.classList.toggle('is-active');
   mobile_menu.classList.toggle('is-active');
   if (menu_btn.classList.contains("is-active")){
-    hamburger_image.src = "images/close_menu.png";
+    hamburger_image.src = "images/close_menu2.png";
   } else{
-    hamburger_image.src = "images/burger_menu.png";
+    hamburger_image.src = "images/burger_menu_2.png";
+  }
+});
+
+
+mobile_nav.addEventListener('click', function () {
+  menu_btn.classList.toggle('is-active');
+  mobile_menu.classList.toggle('is-active');
+  if (menu_btn.classList.contains("is-active")){
+    hamburger_image.src = "images/close_menu2.png";
+  } else{
+    hamburger_image.src = "images/burger_menu_2.png";
   }
 });
 
--- js/gsap_elements.js
+++ js/gsap_elements.js
@@ -44,7 +44,7 @@
           start: "center bottom",
         end: "center center", 
         //   top: "bottom top", 
-          markers: true,
+          // markers: true,
           scrub: true,
         }
       })
@@ -53,19 +53,42 @@
       .from("#info-box-title", { x : innerWidth * -1 })
       .from(".info-box-divided", { x : innerWidth * 1 })
       
-    //     gsap.timeline({
-    //     scrollTrigger: {
-    //       trigger:"#phrase-box",
-    //       start: "center bottom", //Animation start at this point
-    //       top: "center center", //Animation end at this point
-    //       markers: true,
-    //       scrub: true,
-    //     //   pin: true
-    //     }
-    //   })
+        gsap.timeline({
+        scrollTrigger: {
+          trigger:"#phrase-box",
+          start: "center bottom", //Animation start at this point
+          top: "center center", //Animation end at this point
+          // markers: true,
+          scrub: true,
+        //   pin: true
+        }
+      })
 
-    //   .from("#phrase-box", { opacity: 0 })
-    //   .from("#text-phrase-box", { y : innerHeight * 1 })
+      .from("#phrase-box", {
+        duration: 1,
+        scaleX: 0,
+        transformOrigin: "left",
+        ease: "expo.inOut"
+      })
+
+      .from(
+        "#phrase-box",
+        {
+          duration: 2,
+          transformOrigin: "right",
+          ease: "none",
+          css: { color: "black" }
+        },
+        "-=2"
+      );
+
+
+      // .from("#phrase-box", { opacity: 0 })
+
+
+
+      // .from("#text-phrase-box", { y : innerHeight * 1 })
+    
       
 
    
