8e956406d4d131921fdf27caa9f823df6f6ce9f5
MichelleDuque <michelled_1994@hotmail.com>
2023-11-09 23:34:48 -0500

    responsive images were added and xray section changed size

--- js/reviews.js
+++ js/reviews.js
@@ -14,17 +14,26 @@
     const reviews = [
         {
         name: "Michael Smith",
-        image: "images/review_image.png",
+        image: "images/review_image_sop0et_c_scale,w_452.png",
+        image_src_set: "images/review_image_sop0et_c_scale,w_200.png 200w, images/review_image_sop0et_c_scale,w_452.png 452w",
+        image_size: "100vw",
+        image_alt: "Client called Michael Smith",
         reviews_text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,  when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         },
         {
             name: "Daniela Garcia",
-            image: "images/review_image3.png",
+            image: "images/review_image3_pmac2d_c_scale,w_452.png",
+            image_src_set: "images/review_image3_pmac2d_c_scale,w_200.png 200w, images/review_image3_pmac2d_c_scale,w_452.png 452w",
+            image_size: "100vw",
+            image_alt: "Client called Daniela Garcia",
             reviews_text: "Daniela Ipsum is simply dummy text of the printing and typesetting industry.  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,  when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         },
         {
             name: "Eric Levitan",
-            image: "images/review_image2.png",
+            image: "images/review_image2_juw8bb_c_scale,w_452.png",
+            image_src_set: "images/review_image2_juw8bb_c_scale,w_200.png 200w, images/review_image2_juw8bb_c_scale,w_452.png 452w",
+            image_size: "100vw",
+            image_alt: "Client called Eric Levitan",
             reviews_text: "Eric Ipsum is simply dummy text of the printing and typesetting industry.  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,  when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         }
     ]
@@ -39,6 +48,9 @@
         const paragraphReview = document.createElement("p");
     
         imageClient.src = `${reviews[count].image}`;
+        imageClient.srcset = `${reviews[count].image_src_set}`;
+        imageClient.sizes = `${reviews[count].image_size}`;
+        imageClient.alt = `${reviews[count].image_alt}`;
         name_client.textContent = `${reviews[count].name}`;
         paragraphReview.textContent = `${reviews[count].reviews_text}`;
 
