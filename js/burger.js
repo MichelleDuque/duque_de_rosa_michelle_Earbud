1f361470f771202079ab3897837b5b2271aabc60
MichelleDuque <michelled_1994@hotmail.com>
2023-11-10 01:30:41 -0500

    xray mobile was fixed

--- js/burger.js
+++ js/burger.js
@@ -1,9 +1,9 @@
 (() => {
     
 const menu_btn = document.querySelector('.hamburger');
-const mobile_menu = document.querySelector('.mobile-nav');
+const mobile_menu = document.querySelector('.burger_menu');
 const hamburger_image = document.querySelector("#hamburger-image");
-const mobile_nav = document.querySelector(".mobile-nav")
+
 
 menu_btn.addEventListener('click', function () {
   menu_btn.classList.toggle('is-active');
@@ -16,7 +16,7 @@ menu_btn.addEventListener('click', function () {
 });
 
 
-mobile_nav.addEventListener('click', function () {
+mobile_menu.addEventListener('click', function () {
   menu_btn.classList.toggle('is-active');
   mobile_menu.classList.toggle('is-active');
   if (menu_btn.classList.contains("is-active")){
